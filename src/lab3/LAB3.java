6782eb0e774ceb4ba64129fa6ee6ded741c6a25c
Mohamed Al Marashda <b00050674@aus.edu>
2017-11-26 22:45:52 +0400

    Feature b added

--- src/lab3/LAB3.java
+++ src/lab3/LAB3.java
@@ -17,7 +17,7 @@ public class LAB3 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("Hello World");
+        System.out.println("feture b added");
     }
     
 }
